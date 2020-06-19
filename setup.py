d08b3a850b0c5c17a55e396d932e044988ff2512
Eduardo Camarena <lalo.a.camarena@gmail.com>
2020-06-19 17:00:25 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cooler-class',
+    version='0.0.1',
+    description='Cool stuff',
+    author='Eduardo Camarena Santamaria',
+    author_email='lalo.a.camarena@gmail.com',
+    package_dir={'': 'src'},
+    packages=['simple_ml_models'],
+    install_requires=['numpy']
+)
